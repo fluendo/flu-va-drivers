76deb87c5ae4d6d10b9b366f5feab73d2e322d05
Fabian Orccon <cfoch.fabian@gmail.com>
2021-11-18 10:43:11 +0100

    vdpau: Initial scaffolding
    Define an initialize function unimplemented function.
    
    Issue: OCP_2535

---
+++ src/flu_va_drivers_vdpau.c
@@ -0,0 +1,10 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include "flu_va_drivers_vdpau.h"
+
+VAStatus
+FLU_VA_DRIVERS_DRIVER_INIT (VADriverContextP ctx)
+{
+  return VA_STATUS_ERROR_UNKNOWN;
+}
---
+++ src/flu_va_drivers_vdpau.h
@@ -0,0 +1,7 @@
+#ifndef __FLU_VA_DRIVERS_VDPAU_DRV_VIDEO_H__
+#define __FLU_VA_DRIVERS_VDPAU_DRV_VIDEO_H__
+
+#include <va/va.h>
+#include <va/va_backend.h>
+
+#endif /* __FLU_VA_DRIVERS_VDPAU_DRV_VIDEO_H__ */
