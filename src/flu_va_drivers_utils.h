21df454faa03e5e9d2a25b2426ed1101840a018f
Fabián Orccón <forccon@fluendo.com>
2021-12-21 10:48:01 +0100

    vdpau: Implement vaQueryImageFormats
    For now, only NV12 format is supported as this is what Chromium
    suppots, and we want to get an mvp for Chromium.
    
    Issue: OCP-2703

--- src/flu_va_drivers_utils.h
+++ src/flu_va_drivers_utils.h
@@ -4,6 +4,8 @@
 #include <va/va_backend.h>
 #include <stdio.h>
 
+#define FLU_N_ELEMENTS(x) (sizeof (x) / sizeof (x[0]))
+
 void flu_va_drivers_get_vendor (char *str_vendor);
 
 #endif /* __FLU_VA_DRIVERS_UTILS_H__ */
